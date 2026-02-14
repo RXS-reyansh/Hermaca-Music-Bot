d79ba3ce7cad257fb95a8fc9e1b3965aa7d5f622
$ ReYansh <relish.reyansh.489@outlook.com>
2026-02-14 18:21:09 +0530

    Fixed a message bug of noprefix

--- index.js
+++ index.js
@@ -4596,7 +4596,8 @@ async function handlePrefixCommand(message, cmd, args) {
 			case 'nop': {
 				try {
 					if (message.author.id !== ownerId) {
-						return messages.error(message.channel, `${emojis.blackcrown} This command is reserved to bot owner only!`);
+						return messages.error(message.channel, );
+						return message.reply(`${emojis.blackcrown} This command is reserved to bot owner only!`);
 					}
 					const subCmd = args[0] ? args[0].toLowerCase() : null;
 					if (!subCmd) {
